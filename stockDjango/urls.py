390a8b10b86b9dd7645a8cd8455ed74b22c21ae5
CELSO RONALDO DE PAIVA MOURA JUNIOR <celso.junior2@alunos.sc.senac.br>
2023-10-19 21:39:47 -0300

    novo

--- stockDjango/urls.py
+++ stockDjango/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('product-details/<int:id>/', views.product_details, name='product-details')
 ]
--- stockDjango/views.py
+++ stockDjango/views.py
@@ -6,3 +6,7 @@ def index(request):
     for produto in produtos:
         print(produto.name)
     return render(request, 'pages/index.html', {'produtos':produtos})
+
+def product_details(request, id):
+    product = Products.objects.get(id=id)
+    return render(request, 'pages/product_details.html', {'product':product})
