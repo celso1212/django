636f0a812ee278931de9f735b7e126c71849bd23
CELSO RONALDO DE PAIVA MOURA JUNIOR <celso.junior2@alunos.sc.senac.br>
2023-11-01 21:10:30 -0300

    atualização de funcionalidades, botão e campos de busca de produtos, atualização de estoque

--- stockDjango/urls.py
+++ stockDjango/urls.py
@@ -4,5 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('search-product/', views.search_product, name='search_product'),
+    path('add-products/', views.add_product, name='add_product'),
+    path('out_of_stock/', views.out_of_stock, name='out_of_stock'),
+    path('delete-product/<int:id>/', views.delete_product, name='delete-product'),
+    path('sell-product/<int:id>/', views.sell_product, name='sell-product'),
     path('product-details/<int:id>/', views.product_details, name='product-details')
+
 ]
--- stockDjango/views.py
+++ stockDjango/views.py
@@ -1,12 +1,59 @@
-from django.shortcuts import render, HttpResponse
-from .models import Products
+from django.shortcuts import render, HttpResponse, redirect
+from .models import Products, Categories
+from random import randint
+from datetime import datetime
 
 def index(request):
-    produtos = Products.objects.all()
-    for produto in produtos:
-        print(produto.name)
-    return render(request, 'pages/index.html', {'produtos':produtos})
+    produtos = Products.objects.filter(in_stock=True)
+    return render(request, 'pages/index.html', {'produtos': produtos})
+
+def out_of_stock(request):
+    produtos = Products.objects.filter(in_stock=False)
+    return render(request, 'pages/index.html', {'produtos': produtos})
+
+def search_product(request):
+    q = request.GET.get('q')
+    produtos = Products.objects.filter(name__icontains=q)
+    return render (request, 'pages/index.html', {'produtos':produtos}) 
+
+def add_product(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        cod = randint(100, 10000)
+        category = request.POST.get('category')
+        picture = request.FILES.get('picture')
+        price = request.POST.get('price')
+        description = request.POST.get('description')
+        qtd = request.POST.get('qtd')
+        discount = request.POST.get('discount')
+        created_at = datetime.now()
+        in_stock = True
+
+    
+        Products.objects.create(
+            name=name,cod=cod,category_id=category,
+            picture=picture, price=price, description=description,
+            qtd=qtd,discount=discount,created_at=created_at,in_stock=in_stock
+        )
+        return redirect('home')
+    else:
+        categories = Categories.objects.all()
+        return render(request, 'pages/add-product.html', {'categories':categories})
+
 
 def product_details(request, id):
     product = Products.objects.get(id=id)
     return render(request, 'pages/product_details.html', {'product':product})
+
+def delete_product(request, id):
+    product = Products.objects.get(id=id)
+    product.delete()
+    return redirect('home')
+
+def sell_product(request, id):
+    product = Products.objects.get(id=id)
+    product.qtd -= 1
+    if product.qtd == 0:
+        product.in_stock = False
+    product.save()
+    return redirect('product-details', id)
